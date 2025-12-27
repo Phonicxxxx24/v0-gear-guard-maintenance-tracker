7d8f945f05b26faaf44feca945f53575d8fc6edf
khushigaur1204 <khushigaur1204@gmail.com>
2025-12-27 15:20:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
